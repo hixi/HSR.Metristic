cab1d9a06acd3fc6d5e7cdd431a036fc49fa226a
Wasabideveloper <tobias.blaser@hsr.ch>
2016-08-09 18:15:27 -0300

    Refactor w3c request

--- src/check-plugins/html-w3c-validator/html-w3c-validator.ts
+++ src/check-plugins/html-w3c-validator/html-w3c-validator.ts
@@ -17,37 +17,39 @@ export class HtmlW3cValidator implements Check {
 	}
 
 	public execute(directory: string, callback: (report: Report) => {}): void {
-		let reportTemplate = this.reportTemplate;
-		Glob(Path.join(directory,"**/*.html"), null, function (error, filePaths) {
+		Glob(Path.join(directory,"**/*.html"), null, (error, filePaths) => {
 			let waitingForFiles: number = filePaths.length;
-			let reports: { fileName: string, messages: any[] }[] = [];
+			let validations: { fileName: string, messages: any[] }[] = [];
 
 			filePaths.forEach((filePath) => {
 				FS.readFile(filePath, (fileError, fileData) => {
-					let charset: string = DetectCharacterEncoding(fileData).encoding.toLowerCase();
-					Request({
-						url: "https://validator.w3.org/nu/?out=json",
-						method: "POST",
-						headers: {
-							"content-type": "text/html",
-							"charset": charset
-						},
-						body: fileData.toString()
-					}, function (error, response, body){
-						let report = JSON.parse(body);
-						if(report.messages.length > 0) {
-							reports.push({
-								fileName: filePath.replace(directory, ''),
-								messages: report.messages
-							});
+					this.validate(fileData, (validationMessages) => {
+						if(validationMessages.length > 0) {
+							validations.push({ fileName: filePath.replace(directory, ''), messages: validationMessages });
 						}
 						waitingForFiles--;
 						if(waitingForFiles == 0) {
-							callback(new HtmlReport('W3C HTML Validation', reportTemplate, {}, { reports: reports }));
+							callback(new HtmlReport('W3C HTML Validation', this.reportTemplate, {}, { reports: validations }));
 						}
 					});
 				});
 			});
+		});
+	}
+
+	private validate(fileData, callback: (validationMessages: any[]) => void): void {
+		let charset: string = DetectCharacterEncoding(fileData).encoding.toLowerCase();
+		let requestConfiguration: any = {
+			url: "https://validator.w3.org/nu/?out=json",
+			method: "POST",
+			headers: {
+				"content-type": "text/html",
+				"charset": charset
+			},
+			body: fileData.toString()
+		};
+		Request(requestConfiguration, (error, response, body) => {
+			callback(JSON.parse(body).messages);
 		});
 	}
 }
