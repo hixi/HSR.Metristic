443ce9ee9eebe8e599e508dad3ce9a5b5dafa786
Wasabideveloper <tobias.blaser@hsr.ch>
2016-08-10 16:36:47 -0300

    Add tag count metrics

--- src/configuration/profiles.js
+++ src/configuration/profiles.js
@@ -1,5 +1,6 @@
 var StructureMetric = require("../check-plugins/structure-metric/structure-metric").StructureMetric;
 var HtmlW3cValidator = require("../check-plugins/html-w3c-validator/html-w3c-validator").HtmlW3cValidator;
+var HtmlMetric = require("../check-plugins/html-metric/html-metric").HtmlMetric;
 
 
 module.exports = {
@@ -11,7 +12,7 @@ module.exports = {
 	web: {
 		name: 'General web project',
 		description: 'Check HTML, CSS and JS',
-		checks: [StructureMetric, HtmlW3cValidator],
+		checks: [StructureMetric, HtmlW3cValidator, HtmlMetric],
 		options: {}
 	}
 };
