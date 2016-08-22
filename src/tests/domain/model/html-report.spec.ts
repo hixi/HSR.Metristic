367f0d74fdbf3b0031e90fdf3fe128f5571ba8df
Wasabideveloper <tobias.blaser@hsr.ch>
2016-08-22 20:03:12 -0300

    Add more tests for HtmlReport

--- src/tests/domain/model/html-report.spec.ts
+++ src/tests/domain/model/html-report.spec.ts
@@ -2,8 +2,8 @@
 import {HtmlReport} from "../../../domain/model/html-report";
 
 
-describe("HTML Report", function () {
-	it("should render a handlebars template", function () {
+describe("HTML Report", () => {
+	it("should render a handlebars template", () => {
 		let report = new HtmlReport(
 			'Test report',
 			'<h1>{{title}}</h1><p>{{data.test}}</p>',
@@ -11,5 +11,25 @@ describe("HTML Report", function () {
 			{ title: 'abc', data: { test: 'efg' } }
 		);
 		expect(report.renderReport()).toBe('<h1>abc</h1><p>efg</p>');
+	});
+
+	it("should render a partial", () => {
+		let report = new HtmlReport(
+				'Test report',
+				'<p>{{> partial element=title}}</p>',
+				{ partial: '<span>{{element}}</span>' },
+				{ title: 'abc'}
+		);
+		expect(report.renderReport()).toBe('<p><span>abc</span></p>');
+	});
+
+	it("should render the compare helper", () => {
+		let report = new HtmlReport(
+				'Test report',
+				"{{#compare title '==' 'abc'}}<p>it is</p>{{else}}<p>it is not</p>{{/compare}}",
+				{},
+				{ title: 'abc'}
+		);
+		expect(report.renderReport()).toBe('<p>it is</p>');
 	});
 });
