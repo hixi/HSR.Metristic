0b5ddf0286ccaa223e7066ca3f3be56d1c4cb3f9
Wasabideveloper <tobias.blaser@hsr.ch>
2016-08-31 20:12:13 -0300

    Fix check manager to abort not responding executions

--- src/domain/model/check-manager.ts
+++ src/domain/model/check-manager.ts
@@ -13,6 +13,7 @@ export class CheckManager {
 	public execute(profile: Profile, callback: (reports: Report[]) => void) {
 		let reports: Report[] = [];
 		let barrier = new Barrier(profile.checks.length).then(() => { callback(reports); });
+		profile.options['general'] = profile.options['general'] || {};
 
 		profile.checks.forEach((checkConstructor) => {
 			let checkName: string = (<any>checkConstructor).name;
@@ -20,15 +21,25 @@ export class CheckManager {
 				profile.options[checkName] = {}
 			}
 			let check: Check = new checkConstructor(profile.options || {});
+
+			setTimeout(() => {
+				if(!barrier.isFinished(check)) {
+					reports.push(new ErrorReport(checkName, [new Error(`Execution took to long. Aborted.`)]));
+					barrier.finishedTask(check);
+				}
+			}, profile.options['general']['checkTimeout'] || 15000);
+
 			check.execute(this.directory, (report:Report, errors: Error[]) => {
-				if(errors && errors.length > 0) {
-					reports.push(new ErrorReport(checkName, errors));
+				if(!barrier.isFinished(check)) {
+					if (errors && errors.length > 0) {
+						reports.push(new ErrorReport(checkName, errors));
+					}
+					// if there are no checked files there will be no report -> ignore
+					else if (report) {
+						reports.push(report);
+					}
+					barrier.finishedTask(check);
 				}
-				// if there are no checked files there will be no report -> ignore
-				else if (report) {
-					reports.push(report);
-				}
-				barrier.finishedTask(check);
 			});
 		});
 	}
--- src/domain/model/error-report.ts
+++ src/domain/model/error-report.ts
@@ -5,7 +5,9 @@ import {Report} from "./report";
 
 export class ErrorReport implements Report {
 	renderer: any;
-	template: string = `<ul class="list-unstyled">{{#each errors}}<li class="error">{{this.message}}</li>{{/each}}</ul>`;
+	template: string = `<ul class="list-unstyled">{{#each errors}}
+	<li><span class="error label">error</span>{{this.message}}</li>
+{{/each}}</ul>`;
 
 	constructor(public name: string, private errors: Error[]) {
 		this.renderer = Handlebars.compile(this.template);
