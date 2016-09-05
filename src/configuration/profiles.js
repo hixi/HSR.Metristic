48d3dfa6cf832eadda735b755a669ea84bf6d5b8
Wasabideveloper <tobias.blaser@hsr.ch>
2016-09-05 11:43:35 -0300

    Hreflang attribute rule

--- src/configuration/profiles.js
+++ src/configuration/profiles.js
@@ -25,9 +25,34 @@ module.exports = {
 		options: {}
 	},
 	webCheckAdvanced: {
-		name: 'Extensive web project checking',
+		name: 'Extensive web project check',
 		description: 'Show metrics of HTML, CSS and JS and check JS code style and check by custom rules and validate html by W3C',
 		checks: [StructureMetric, HtmlMetric, CssMetric, JsStyleCheck, RegexCheck, HtmlW3cValidator],
 		options: {}
+	},
+	WED1Testation: {
+		name: "WED1 Testation check",
+		description: "Check HTML, CSS and JS of WED1 testation.",
+		checks: /*[StructureMetric, HtmlMetric, CssMetric, JsStyleCheck,*/ [RegexCheck],
+		options: {
+			RegexCheck: {
+				rules: [
+					{
+						name: "Hreflang attribute for external links",
+						files: "index.html",
+						snippet: {
+							rule: /<a[^<>]*hreflang="\w{2}"[^<>]*>[^<>\/]*<\/a>/igm,
+							min: 2, // min: null means bound will not be checked
+							max: 4, // max: null means bound will not be checked
+							error: {
+								message: "Not enough links with hreflang attribute found.",
+								type: "warning", // "info" | "warning" | "error"
+								hideOccurrencesInReport: true
+							}
+						}
+					}
+				]
+			}
+		}
 	}
 };
