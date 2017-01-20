faaa0e2d1bb160ecd16fe4190bcda50630138bf4
mgfeller <mgfeller@hsr.ch>
2017-01-20 13:25:53 +0100

    added windows support: tmp path changed, line counter and removed encoding

--- configuration/app.js
+++ configuration/app.js
@@ -1,8 +1,8 @@
 var Path = require('path');
-
+var os = require('os');
 
 module.exports = {
-	APP_PORT: process.env.APP_PORT || 8080,
-	MAX_UPLOAD_SIZE: process.env.MAX_UPLOAD_SIZE || 5, // MB
-	ARCHIVE_TMP_DIRECTORY: process.env.ARCHIVE_TMP_DIRECTORY || '/tmp/metristic/'
+    APP_PORT: process.env.APP_PORT || 8080,
+    MAX_UPLOAD_SIZE: process.env.MAX_UPLOAD_SIZE || 5, // MB
+    ARCHIVE_TMP_DIRECTORY: process.env.ARCHIVE_TMP_DIRECTORY || `${os.tmpdir()}/metristic/`
 };
