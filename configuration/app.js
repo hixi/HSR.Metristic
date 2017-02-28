9007e0d59461d9454fe8116037e9423aefc75018
mgfeller <mgfeller@hsr.ch>
2017-02-28 10:31:33 +0100

    added cluster support

--- configuration/app.js
+++ configuration/app.js
@@ -2,6 +2,7 @@ var Path = require('path');
 var os = require('os');
 
 module.exports = {
+    MAX_FORKS: 2,
     APP_PORT: process.env.APP_PORT || 8080,
     MAX_UPLOAD_SIZE: process.env.MAX_UPLOAD_SIZE || 5, // MB
     ARCHIVE_TMP_DIRECTORY: process.env.ARCHIVE_TMP_DIRECTORY || `${os.tmpdir()}/metristic/`
